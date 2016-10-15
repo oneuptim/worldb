72171d968575bca323541e935a5431f236bdcf67
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2016-10-15 17:44:25 -0500

    sql and db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if Country.count.zero? && State.count.zero? && City.count.zero? 
+  sql = File.read(Rails.root.join('db', "worldb.sql"))
+  statements = sql.split(/;$/)
+  statements.pop
+
+  ActiveRecord::Base.transaction do
+    statements.each do |statement|
+      ActiveRecord::Base.connection.execute(statement)
+    end
+  end
+end
