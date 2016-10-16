f3391eed738810e5dd9ba1a39b78ea25e297c071
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2016-10-15 19:02:17 -0500

    no seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-if Country.count.zero? && State.count.zero?
-  sql = File.read(Rails.root.join('db', "worldb.sql"))
-  statements = sql.split(/;$/)
-  statements.pop
-
-  ActiveRecord::Base.transaction do
-    statements.each do |statement|
-      ActiveRecord::Base.connection.execute(statement)
-    end
-  end
-end
+# if Country.count.zero? && State.count.zero?
+#   sql = File.read(Rails.root.join('db', "worldb.sql"))
+#   statements = sql.split(/;$/)
+#   statements.pop
+#
+#   ActiveRecord::Base.transaction do
+#     statements.each do |statement|
+#       ActiveRecord::Base.connection.execute(statement)
+#     end
+#   end
+# end
